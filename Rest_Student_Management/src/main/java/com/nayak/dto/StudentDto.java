81fcc3f23bd22d3fa175105155bd65018d889588
Ujjawal Nayak <ujjawal.nayak07@gmail.com>
2026-04-07 22:06:46 +0530

    Dto file created

---
+++ Rest_Student_Management/src/main/java/com/nayak/dto/StudentDto.java
@@ -0,0 +1,11 @@
+package com.nayak.dto;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class StudentDto {
+
+	private String name;
+	private int marks;
+	private String house;
+}
