4493c9fdd4e3d958c8d18dade4bcc18189dea95d
UjjawalNayak <ujjawal.nayak07@gmail.com>
2026-04-10 18:49:42 +0530

    Dto classes added

--- Rest_Student_Management/src/main/java/com/nayak/dto/StudentDto.java
+++ Rest_Student_Management/src/main/java/com/nayak/dto/StudentDto.java
@@ -5,7 +5,9 @@ import org.springframework.stereotype.Component;
 @Component
 public class StudentDto {
 
+	private int roll;
 	private String name;
 	private int marks;
 	private String house;
+	private boolean active;
 }
